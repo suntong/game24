bcceab251eefb7109176c27690efae42eaff44d2
Tong Sun <suntong@cpan.org>
2016-02-15 10:13:30 -0500

    - [*] no fraction allowed during calculation

--- game24.go
+++ game24.go
@@ -26,16 +26,12 @@ const (
 	op_div
 )
 
-type frac struct {
-	num, denom int
-}
-
 // Expr is for Expression: it can either be a single number, or a result of
 // binary operation from left and right node
 type Expr struct {
 	op          int
 	left, right *Expr
-	value       frac
+	value       int
 }
 
 var n_cards = 4
@@ -45,7 +41,7 @@ var digit_range = 9
 // String will convert the expression tree into infix expression string.
 func (x *Expr) String() string {
 	if x.op == op_num {
-		return fmt.Sprintf("%d", x.value.num)
+		return fmt.Sprintf("%d", x.value)
 	}
 
 	var bl1, br1, bl2, br2, opstr string
@@ -79,35 +75,34 @@ func (x *Expr) String() string {
 		bl2 + x.right.String() + br2
 }
 
-func expr_eval(x *Expr) (f frac) {
+func expr_eval(x *Expr) (ret int, ok bool) {
 	if x.op == op_num {
-		return x.value
+		return x.value, true
 	}
 
-	l, r := expr_eval(x.left), expr_eval(x.right)
+	l, lok := expr_eval(x.left)
+	r, rok := expr_eval(x.right)
+	if !(lok && rok) {
+		return 0, false
+	}
 
 	switch x.op {
 	case op_add:
-		f.num = l.num*r.denom + l.denom*r.num
-		f.denom = l.denom * r.denom
-		return
+		return l + r, true
 
 	case op_sub:
-		f.num = l.num*r.denom - l.denom*r.num
-		f.denom = l.denom * r.denom
-		return
+		return l - r, true
 
 	case op_mul:
-		f.num = l.num * r.num
-		f.denom = l.denom * r.denom
-		return
+		return l * r, true
 
 	case op_div:
-		f.num = l.num * r.denom
-		f.denom = l.denom * r.num
-		return
+		if r == 0 || l%r != 0 {
+			return 0, false
+		}
+		return l / r, true
 	}
-	return
+	return 0, false
 }
 
 // Solve is key function to solve the 24 game
@@ -115,8 +110,8 @@ func Solve(ex_in []*Expr) bool {
 	// only one expression left, meaning all numbers are arranged into
 	// a binary tree, so evaluate and see if we get 24
 	if len(ex_in) == 1 {
-		f := expr_eval(ex_in[0])
-		if f.denom != 0 && f.num == f.denom*goal {
+		r, ok := expr_eval(ex_in[0])
+		if ok && r == goal {
 			fmt.Println(ex_in[0].String())
 			return true
 		}
@@ -174,9 +169,8 @@ func Play() {
 
 	for k := 0; k < 10; k++ {
 		for i := 0; i < n_cards; i++ {
-			cards[i] = &Expr{op_num, nil, nil,
-				frac{rand.Intn(digit_range-1) + 1, 1}}
-			fmt.Printf(" %d", cards[i].value.num)
+			cards[i] = &Expr{op_num, nil, nil, rand.Intn(digit_range-1) + 1}
+			fmt.Printf(" %d", cards[i].value)
 		}
 		fmt.Print(":  ")
 		if !Solve(cards) {
