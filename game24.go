6a2ac5e56db27def26164f87a59c7ed4447c1c5b
Tong Sun <suntong@cpan.org>
2016-02-15 17:20:46 -0500

    - [*] move package doc up

--- game24.go
+++ game24.go
@@ -1,13 +1,13 @@
+/*
+package game24 provides go solution to the 24 game
+*/
+
 ////////////////////////////////////////////////////////////////////////////
 // Porgram: game24.go
 // Purpose: Play the 24 game (http://rosettacode.org/wiki/24_game)
 // Authors: Tong Sun (c) 2016, All rights reserved
 // Credits: http://rosettacode.org/wiki/24_game/Solve#Go
 ////////////////////////////////////////////////////////////////////////////
-
-/*
-package game24 provides fast solution to the 24 game
-*/
 
 package game24
 
