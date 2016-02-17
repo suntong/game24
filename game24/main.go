5a227cf4ef369fb808f981f4584e2dd464e77b70
Tong Sun <suntong@cpan.org>
2016-02-17 12:13:18 -0500

    - [!] bug fix for golint: dont use underscores

--- game24/main.go
+++ game24/main.go
@@ -18,17 +18,17 @@ import "github.com/suntong/game24"
 ////////////////////////////////////////////////////////////////////////////
 // Global variables definitions
 
-const n_cards = 4
-const digit_range = 9
+const nCards = 4
+const digitRange = 9
 
 // Play is for playing the game
 func Play() {
-	cards := make([]*game24.Expr, n_cards)
+	cards := make([]*game24.Expr, nCards)
 	rand.Seed(time.Now().Unix())
 
 	for k := 0; k < 30; k++ {
-		for i := 0; i < n_cards; i++ {
-			cards[i] = game24.NewExpr(rand.Intn(digit_range) + 1)
+		for i := 0; i < nCards; i++ {
+			cards[i] = game24.NewExpr(rand.Intn(digitRange) + 1)
 			fmt.Printf(" %d", cards[i].Value())
 			if i == 1 {
 				fmt.Print("\n")
