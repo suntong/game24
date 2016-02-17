ef40ca2e1bbfab2e25bc1ead767f64c7f8a671ae
Tong Sun <suntong@cpan.org>
2016-02-17 12:09:41 -0500

    - [!] bug fix for golint: package comment should be of the form "Package ..."

--- game24.go
+++ game24.go
@@ -6,7 +6,7 @@
 ////////////////////////////////////////////////////////////////////////////
 
 /*
-package game24 provides fast solution to the 24 game (http://rosettacode.org/wiki/24_game).
+Package game24 provides fast solution to the 24 game (http://rosettacode.org/wiki/24_game).
 
 In brief, the 24 game is a card game that, with a given four cards (numbers, each from 1 to 9), create an expression using any elementary operators (+, -, *, /), containing only these numbers, exactly once each, such that the result is 24.
 */
